c0573b4fc1ff0d65ed13b30652ed2b9c69feae92
Gabriel Rodrigues <gabrielrodriguesgithub>
2025-04-06 18:34:07 -0300

    finished project

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,30 @@
-let button = document.querySelector('button')
-function click() {
-    alert('oiiii')
-}
+let vermelho = document.querySelector('.Vermelho')
+let azul = document.querySelector('.azul')
+let amarelo = document.querySelector('.amarelo')
+let rosa = document.querySelector('.rosa')
+let verde = document.querySelector('.verde')
+let roxo = document.querySelector('.roxo')
+let container = document.querySelector('.container')
+function clickVermelho() {
+    container.style.backgroundColor = 'red'
 
+}
+function clickAzul() {
+    container.style.backgroundColor = 'blue'
+}
+function clickAzul() {
+    container.style.backgroundColor = 'blue'
+}
+function clickAmarelo() {
+    container.style.backgroundColor = 'yellow'
+}
+function clickRosa() {
+    container.style.backgroundColor = 'pink'
+}
+function clickVerde() {
+    container.style.backgroundColor = 'green'
+}
+function clickRoxo() {
+    container.style.backgroundColor = 'purple'
+}
 button.addEventListener('click', click)
